4323d41c0f89a7972dfe328456d2d833beec9840
Zakadev <Zakaria.xperia.z2@gmail.com>
2021-04-25 20:38:36 +0200

    Primitive grid and fonts added

--- lib/Widgets/categories_screen.dart
+++ lib/Widgets/categories_screen.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
+import 'category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Zaka Recommends'),
       ),
-      children: [],
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((e) => CategoryItem(
+                  e.title,
+                  e.color,
+                ))
+            .toList(),
+      ),
     );
   }
 }
